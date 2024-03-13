4ff342a0f0e914ee12ad2aea6168684324650f9c
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-03-13 13:43:15 -0300

    feat: Header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
-import type { AppProps } from "next/app";
-import { globalStyles } from "../styles/global";
+import type { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+import logoImg from '@/src/assets/logo.svg'
+import { Header, Container } from "../styles/pages/app"
+import Image from "next/image"
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
