2f932e5923a92a79b5251beb15ca9e981774e58a
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-03-17 16:06:39 -0300

    feat: Product page structure

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,25 @@
+import { ImageContainer, ProductContainer, ProductDetails } from "@/src/styles/pages/product"
 import { useRouter } from "next/router"
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Product: {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer>
+
+      </ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 59,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa ea mollitia aperiam natus vero commodi a, sunt inventore perspiciatis quia quis veniam dolor iusto pariatur alias debitis magnam officiis esse.</p>
+
+        <button>
+          Comprar Agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
---
+++ src/styles/pages/product.ts
@@ -0,0 +1,69 @@
+import { styled } from "@stitches/react";
+
+export const ProductContainer = styled('main', {
+  display: "grid",
+  gridTemplateColumns: "repeat(2, 1fr)",
+  alignItems: "stretch",
+  gap: '4rem',
+
+  maxWidth: 1180,
+  margin: '0 auto'
+})
+
+export const ImageContainer = styled('div', {
+  width: '100%',
+  maxWidth: 576,
+  height: 'calc(656px - 0.5rem)',
+  background: 'linear-gradient(180deg, #1ea483 0%, #7465d4 100%)',
+  borderRadius: 8,
+  padding: '0.125rem',
+
+  display: "flex",
+  justifyContent: 'center',
+  alignItems: 'center',
+
+  img: {
+    objectFit: "cover"
+  }
+})
+
+export const ProductDetails = styled('div', {
+  display: "flex",
+  flexDirection: "column",
+
+  h1: {
+    fontSize: '$2xl',
+    color: '$gray300',
+  },
+
+  span: {
+    marginTop: '1rem',
+    display: 'block',
+    fontSize: '$2xl',
+    color: '$green300'
+  },
+
+  p: {
+    marginTop: '2.5rem',
+    fontSize: '$md',
+    lineHeight: 1.6,
+    color: '$gray300'
+  },
+
+  button: {
+    marginTop: 'auto',
+    backgroundColor: '$green500',
+    border: 0,
+    color: '$white',
+    borderRadius: 8,
+    padding: '1.25rem',
+    cursor: 'pointer',
+    fontWeight: 'bold',
+    fontSize: '$md',
+    transition: '0.3s',
+
+    '&:hover': {
+      backgroundColor: '$green300',
+    }
+  }
+})
