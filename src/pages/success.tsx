4183fdb23595e341366bcf5298fb95697b707e0e
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-03-19 15:57:10 -0300

    feat: Success page customer data

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,18 +1,31 @@
 import Image from "next/image";
 import { ImageContainer, SuccessContainer } from "../styles/pages/success";
 import Link from "next/link";
+import { GetServerSideProps } from "next";
+import { stripe } from "../lib/stripe";
+import Stripe from "stripe";
 
-export default function Success() {
+interface productPurschaseDetailType {
+  productPurschaseDetail: {
+    customerName: string,
+    product: {
+      name: string,
+      imageUrl: string
+    }
+  }
+}
+
+export default function Success({ productPurschaseDetail }: productPurschaseDetailType) {
   return (
     <SuccessContainer>
       <h1>Compra efetuada!</h1>
 
       <ImageContainer>
-        {/* <Image src={} alt=""/> */}
+        <Image src={productPurschaseDetail.product.imageUrl} width={120} height={110} alt=""/>
       </ImageContainer>
 
       <p>
-        Parabéns pela aquisição <strong>*nome do cliente*</strong>! Sua <strong>*nome da camisa*</strong> será enviada logo logo !
+        Parabéns pela aquisição <strong>{productPurschaseDetail.customerName}</strong>! Sua <strong>{productPurschaseDetail.product.name}</strong> será enviada logo logo !
       </p>
 
       <Link href='/'>
@@ -20,4 +33,28 @@ export default function Success() {
       </Link>
     </SuccessContainer>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const sessionId = String(query.session_id)
+
+  const session = await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product']
+  })
+
+  const product = session.line_items.data[0].price.product as Stripe.Product
+
+  const productPurschaseDetail = {
+    customerName: session.customer_details.name,
+    product: {
+      name: product.name,
+      imageUrl: product.images[0]
+    }
+  }
+
+  return {
+    props: {
+      productPurschaseDetail
+    }
+  }
 }
